44031bee01efb425f2d0a8d1d5cd6a984e709414
Badr Alnassar <badrabdulaziz.24@gmail.com>
2019-08-09 03:47:44 +0300

    added leaderboard page

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+function anima() {
+    $(".leaderboard").animate({ top: '63%' }, "slow");
+    $(".current1").fadeIn("slow");
+    $(".time").fadeIn("slow");
+    $(".current2").fadeIn("slow");
+}
+function returnanima() {
+    $(".current1").fadeOut("slow");
+    $(".time").fadeOut("slow");
+    $(".current2").fadeOut("slow");
+    $(".leaderboard").animate({ top: '44%' }, "slow");
+}
+
+
+
+
